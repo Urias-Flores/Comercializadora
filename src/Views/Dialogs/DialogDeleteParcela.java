a375d41a545ae66de2dd4dea7d196441db8208ae
M3NDOZA9714 <110438449+M3NDOZA9714@users.noreply.github.com>
2022-08-05 00:00:37 -0600

    texto

--- src/Views/Dialogs/DialogDeleteParcela.java
+++ src/Views/Dialogs/DialogDeleteParcela.java
@@ -115,7 +115,7 @@ public class DialogDeleteParcela extends javax.swing.JDialog {
 
         jLabel1.setFont(new java.awt.Font("Cascadia Code", 0, 19)); // NOI18N
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("¿Desea salir y cerrar su sesion actual?");
+        jLabel1.setText("¿Desea Eliminar la Parcela?");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
