b021800a6f6bc58e1fd58704105167bcb73d18d0
Urias-Flores <93843706+Urias-Flores@users.noreply.github.com>
2022-08-06 17:45:11 -0600

    Update 17:45

--- src/Views/Panels/Inicio/Facturacion.java
+++ src/Views/Panels/Inicio/Facturacion.java
@@ -13,44 +13,21 @@ public class Facturacion extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
-
         setBackground(new java.awt.Color(255, 255, 255));
-
-        jButton1.setText("LLAMAR");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(145, 145, 145)
-                .addComponent(jButton1)
-                .addContainerGap(786, Short.MAX_VALUE))
+            .addGap(0, 1099, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(235, 235, 235)
-                .addComponent(jButton1)
-                .addContainerGap(328, Short.MAX_VALUE))
+            .addGap(0, 634, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        boolean result = Dialogs.ShowLogerSecureDialog("La eliminacion de un usuairo ", 
-                                        "para eliminar todos los usuario de", 
-                                        "Ingrese su contraseña y escriba la la pae");
-        System.out.print(result);
-    }//GEN-LAST:event_jButton1ActionPerformed
-
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
     // End of variables declaration//GEN-END:variables
 }
