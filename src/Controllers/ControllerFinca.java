6f5087a95cf884581e7d93296e694062fd1985c2
M3NDOZA9714 <110438449+M3NDOZA9714@users.noreply.github.com>
2022-08-05 22:39:45 -0600

    Update ControllerFinca.java

--- src/Controllers/ControllerFinca.java
+++ src/Controllers/ControllerFinca.java
@@ -2,22 +2,23 @@ package Controllers;
 
 import Models.ModelFinca;
 import Models.ModelProductor;
+import java.util.ArrayList;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
 public class ControllerFinca {
-    
+
     private ModelFinca modelFinca = new ModelFinca();
     private ModelProductor modelProductor = new ModelProductor();
-    
+
     private JTextField FincaID;
     private JTextField Nombre;
     private JTextField ProductorID;
     private JTextField Ubicacion;
 
     public ControllerFinca() {
-        
+
     }
 
     public ControllerFinca(JTextField FincaID, JTextField Nombre, JTextField ProductorID, JTextField Ubicacion) {
@@ -32,22 +33,27 @@ public class ControllerFinca {
         this.ProductorID = ProductorID;
         this.Ubicacion = Ubicacion;
     }
-    
-    public DefaultTableModel SelectModelFinca(){
+
+    public DefaultTableModel SelectModelFinca() {
         return modelFinca.SelectModelFinca();
     }
-    
-    public DefaultComboBoxModel setFincaCmb () {
+
+    public DefaultComboBoxModel setFincaCmb() {
         return modelFinca.SelectModelFincaCmb();
-    } 
-    
-    public boolean InsertFinca(){
+    }
+
+    public ArrayList selectFincaPorID(int FincaId) {
+        modelFinca.setFincaID(FincaId);
+        return modelFinca.SelectFincaPorID();
+    }
+
+    public boolean InsertFinca() {
         modelFinca.setNombre(Nombre.getText());
         modelFinca.setProductorID(Integer.parseInt(ProductorID.getName()));
         modelFinca.setUbicacion(Ubicacion.getText());
         return modelFinca.InsertFinca();
     }
-    
+
     public boolean InsertFinca(int id, String nombre, String ubicacion) {
         modelFinca.setProductorID(id);
         modelFinca.setNombre(nombre);
@@ -55,6 +61,14 @@ public class ControllerFinca {
         return modelFinca.InsertFinca();
     }
     
+    public boolean UpdateFinca(int FincaID, int ProductorID, String Nombre, String Ubicacion) {
+        modelFinca.setFincaID(FincaID);
+        modelFinca.setProductorID(ProductorID);
+        modelFinca.setNombre(Nombre);
+        modelFinca.setUbicacion(Ubicacion);
+        return modelFinca.UpdateFinca();
+    }
+
     public boolean DeleteFinca(int FincaId) {
         modelFinca.setFincaID(FincaId);
         return modelFinca.DeleteFinca();
