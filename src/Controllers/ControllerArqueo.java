d939ceb6bbc2ab8e19e4cfb5630f1922680081f3
M3NDOZA9714 <110438449+M3NDOZA9714@users.noreply.github.com>
2022-08-10 15:31:49 -0600

    ,

--- src/Controllers/ControllerArqueo.java
+++ src/Controllers/ControllerArqueo.java
@@ -1,6 +1,34 @@
 
 package Controllers;
 
+import Models.ModelArqueo;
+
 public class ControllerArqueo {
+    private ModelArqueo mArqueo = new ModelArqueo();
     
+    public float ObtenerSaldoAnterior() {
+        return mArqueo.ObtenerSaldoAnterior();
+    }
+    
+    public float ObtenerSaldoFinal() {
+        return mArqueo.ObtenerSaldoFinal();
+    }
+    
+    public float ObtenerTotalCompras() {
+        return mArqueo.ObtenerTotalCompras();
+    }
+    
+    public float ObtenerTotalVentas() {
+        return mArqueo.ObtenerTotalVentas();
+    }
+    
+    public boolean InsertArqueo(int usuarioID, float SaldoAnterior, float SaldoFinalSistema, float SaldoFinalUsuario, float Diferencia, String Estado) {
+        mArqueo.setUsuarioID(usuarioID);
+        mArqueo.setSaldoAnterior(SaldoAnterior);
+        mArqueo.setSaldoFinalSistema(SaldoFinalSistema);
+        mArqueo.setSaldoFinalUsuario(SaldoFinalUsuario);
+        mArqueo.setDiferencia(Diferencia);
+        mArqueo.setEstado(Estado);
+        return mArqueo.InsertArqueo();
+    }
 }
